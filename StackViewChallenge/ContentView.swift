ad53090108a5dd1a5b5f39568ed38687750fa24b
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-03 11:39:26 +0700

    first commit

--- StackViewChallenge/ContentView.swift
+++ StackViewChallenge/ContentView.swift
@@ -9,13 +9,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack (spacing: 20){
+            VStack {
+                Text("Instant Developer")
+                    .fontWeight(.medium)
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+                
+                Text("Get help from experts in 15 minutes")
+                    .foregroundColor(.white)
+            }
+            
+            HStack (alignment: .bottom, spacing: 10) {
+                Image("student")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("tutor")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding(.horizontal, 60)
+            
+            Text("Need help with coding problems? Register!")
+                .foregroundColor(.white)
+            
+            Spacer()
+            
+            VSignUpButtonGroup()
         }
-        .padding()
+        //.padding(.top, 30)
+        
+        .background {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+        }
+    }
+}
+
+struct VSignUpButtonGroup: View {
+    var body: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.indigo)
+            .cornerRadius(10)
+            
+            Button {
+                
+            } label: {
+                Text("Log In")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.gray)
+            .cornerRadius(10)
+        }
     }
 }
 
@@ -24,3 +82,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
