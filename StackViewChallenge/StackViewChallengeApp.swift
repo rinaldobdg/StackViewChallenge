f3868a01e7dd8e9557e870f4ca9e0c112aac4ebf
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-03 09:53:14 +0700

    Initial Commit

---
+++ StackViewChallenge/StackViewChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewChallengeApp.swift
+//  StackViewChallenge
+//
+//  Created by Benny Rinaldo on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StackViewChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
